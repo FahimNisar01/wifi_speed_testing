5339a3c395f0aebd0edb784f257698213229c1e7
Fahim Nisar <nisarfaheem38@gmail.com>
2023-08-24 09:45:40 +0500

    wifi_speed_test_app

---
+++ app/src/main/java/com/example/wifispeedtest/UIState.kt
@@ -0,0 +1,11 @@
+package com.example.wifispeedtest
+
+
+class UIState (
+    val speed:String="",
+    val ping:String="-",
+    val maxSpeed:String="-",
+    val arcValue:Float=0f,
+    val inProgress:Boolean=false
+
+)
